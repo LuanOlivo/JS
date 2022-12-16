e7e2724ff8365a825cdb9495b18a777742c1d7cf
Luan Olivo <luanolivo@outlook.com>
2022-12-16 11:34:36 -0300

    update js

--- POO/index.js
+++ POO/index.js
@@ -1,9 +1,9 @@
 // Usado para avisar ao navegador que toda variavel precisa ser declarada -- uso global -- 
 'use strict'
 
-// DECLARAÇÃO DE UMA CLASSE
+// DECLARAÇÃO DE UMA CLASSE 
 class Livro {
-    
+
 }
 
 class Equipamento{
