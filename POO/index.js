5d8e23e8739af10874d076bef85b81b9f46b8212
Luan Olivo <luanolivo@outlook.com>
2022-12-18 11:45:39 -0300

    Update

--- POO/index.js
+++ POO/index.js
@@ -11,13 +11,49 @@ class Filme {
 }
 
 let meuFilme = new Filme('Aprendendo JS', 2022); // (conteudo declarado  como argumento )
-console.log(meuFilme);
+//console.log(meuFilme);
 
 class Produto {
     constructor(){ // metodo construtor sem parametros
         this.tipo = 'produto x'; // atributo declarado 
     }
 }
-
 const prod = new Produto(); // declaraão de um objeto
-console.log(prod.tipo) // Chamada do objeto
+//console.log(prod.tipo) // Chamada do objeto
+
+// DECLARAÇÃO DE ATRIBUTOS
+class Financimaneto{
+    valorFinaciamento; // atributp 
+    taxaJuros;// atributo
+    numero = 0; // atributo
+    // O atributo declarado na class e declarado no metodo construtor sobreescreve o valor 
+    constructor(valorFinaciamento, taxaJuros, numero /*Parametros */){ // metodo construtor
+        this.valorFinaciamento = valorFinaciamento;
+        this.taxaJuros = taxaJuros;
+        this.numero = numero;
+    }
+
+}
+
+const fin = new Financimaneto(10000, 4, 7/*Argumentos*/);
+//console.log(fin);
+
+// INSTANCIANDO UM OBJETO EM JS
+
+class Disciplina{
+    codigo;
+    nome;
+    constructor(codigo, nome){
+        this.codigo = codigo;
+        this.nome = nome;
+    }
+}
+// F2 muda o nome do atributo ou variavel em todo o codigo 
+
+// instaciando um objeto na class disciplina
+
+const disc1 = new Disciplina(123, 'Matematica');
+// console.log(disc1)
+// console.log(disc1.nome) // exibição apenas do atributo nome deste objeto
+
+// ATRIBUIÇÃO DE OBJETO EM  JS
